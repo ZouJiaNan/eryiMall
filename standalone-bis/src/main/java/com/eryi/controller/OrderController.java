1c2f57dc20524dc57d2afa1cc413350caeb372ea
18008150802 <2512526610@qq.com>
2024-12-16 14:24:07 +0800

    调整项目结构

--- standalone-bis/src/main/java/com/eryi/controller/OrderController.java
+++ standalone-bis/src/main/java/com/eryi/controller/OrderController.java
@@ -3,15 +3,12 @@ package com.eryi.controller;
 import com.eryi.domain.Order;
 import com.eryi.dto.ResultBean;
 import com.eryi.service.InventoryService;
-import com.eryi.service.ProductService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.messaging.support.MessageBuilder;
 
 @RestController("BisOrderController")
 @RequestMapping("/bis/order")
@@ -39,7 +36,7 @@ public class OrderController extends BaseController{
         try {
             String json = objectMapper.writeValueAsString(order);
             rocketMQTemplate.send(topic,MessageBuilder.withPayload(json).build());
-        } catch (JsonProcessingException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
         return success(order);
